a449d7f3b4c78d9a5f3a8732aa9e1e72287da05a
roonleng <42977073+roonleng@users.noreply.github.com>
2018-09-26 13:39:17 -0400

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author rlong19
+ *
+ */
+public class BankAccount 
+{
+	private String name;
+	private int accNum;
+	private double balance;
+	public BankAccount()
+	{
+		name
+	}
+}
+https://github.com/roonleng/BankAccount/wiki
