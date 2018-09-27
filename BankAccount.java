9f66d597d8df1fe1372486c5ea9bbe280f69e94c
roonleng <42977073+roonleng@users.noreply.github.com>
2018-09-26 23:11:16 -0400

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -10,7 +10,35 @@ public class BankAccount
 	private double balance;
 	public BankAccount()
 	{
-		name
+		name= "Ryan";
+		accNum= 12345;
+		balance= 100000
 	}
+	public BankAccount(String a, int b, 0)
+	{
+		name= a;
+		accNum= b;
+	}
+	public BankAccount(String a, int b, double c)
+	{
+		name= a;
+		accNum= b;
+		balance= c;
+	}
+	public void deposit(double deposit)
+	{
+		balance= balance-deposit
+	}
+	public double getBalance()
+	{
+		return balance
+	}
+	public String toString()
+	{
+		return "Balance: " + balance;
+		return "\n Name: " + name; 
+		return "\n Account Number: " + accNum
+	}
+	
 }
 https://github.com/roonleng/BankAccount/wiki
